080a59516cf1589b8ae2f1f960380251b94ee69a
Jsanto84 <66800192+Jsanto84@users.noreply.github.com>
2020-06-14 23:35:02 +0300

    Add files via upload
    If-else( nested if-else) calculate area of rectangles and compare

---
+++ P3T1_AreasOfRectangles_JoshuaSantobianco.py
@@ -0,0 +1,25 @@
+# Calculate area of two rectangles and determine which one is larger or if they're the same size
+# 14 June 2020
+# CTI-110 P3T1-Area of Rectangles
+# Joshua Santobianco
+
+# Get the dimensions fo rectangle 1.
+length1 = int(input('Enter the length of rectangle 1: '))
+width1 = int(input('Enter the width of rectangle 1: '))
+
+# Get the dimensions of rectangle 2.
+length2 = int(input('Enter the length of rectangle 2: '))
+width2 = int(input('Enter the width of rectangle 2: '))
+
+# Calculate the areas of the rectangles.
+area1 = length1 * width1
+area2 = length2 * width2
+
+# Determine which has the greater area.
+if area1 > area2:
+    print('Rectangle 1 has the greater area.')
+else:
+    if area2 > area1:
+        print('Rectangle 2 has the greater area.')
+    else:
+        print('Both have the same area.')
